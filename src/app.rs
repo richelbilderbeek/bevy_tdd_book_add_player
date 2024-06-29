582efca366826d404321909815b82e88f6dfd711
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-06-29 15:49:52 +0200

    EOD

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,32 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-pub struct Paddle;
+pub struct Player;
 
 /// Create a Bevy app
 pub fn create_app() -> App {
     let mut app = App::new();
-    app.add_systems(Startup, setup_paddle);
+    app.add_systems(Startup, setup_player);
     app.update();
     return app;
 }
 
-// Add the paddle to our world
-fn setup_paddle(mut commands: Commands) {
-    commands.spawn((SpriteBundle { ..default() }, Paddle));
+// Add the player to our world
+fn setup_player(mut commands: Commands) {
+    commands.spawn(Player);
+}
+
+// Add the player to our world
+fn setup_player_with_sprite(mut commands: Commands) {
+    commands.spawn((SpriteBundle { ..default() }, Player));
 }
 
 #[cfg(test)]
-fn count_n_paddles(app: &App) -> usize {
+fn count_n_players(app: &App) -> usize {
     let mut n = 0;
     for c in app.world.components().iter() {
-        // The complete name will be '[crate_name]::Paddle'
-        if c.name().contains("Paddle") {
+        // The complete name will be '[crate_name]::Player'
+        if c.name().contains("Player") {
             n += 1;
         }
     }
@@ -43,23 +48,23 @@ mod tests {
     }
 
     #[test]
-    fn test_empty_app_has_no_paddles() {
+    fn test_empty_app_has_no_players() {
         let app = App::new();
-        assert_eq!(count_n_paddles(&app), 0);
+        assert_eq!(count_n_players(&app), 0);
     }
 
     #[test]
-    fn test_setup_paddle_adds_a_paddle() {
+    fn test_setup_player_adds_a_player() {
         let mut app = App::new();
-        assert_eq!(count_n_paddles(&app), 0);
-        app.add_systems(Startup, setup_paddle);
+        assert_eq!(count_n_players(&app), 0);
+        app.add_systems(Startup, setup_player);
         app.update();
-        assert_eq!(count_n_paddles(&app), 1);
+        assert_eq!(count_n_players(&app), 1);
     }
 
     #[test]
-    fn test_create_app_has_a_paddle() {
+    fn test_create_app_has_a_player() {
         let app = create_app();
-        assert_eq!(count_n_paddles(&app), 1);
+        assert_eq!(count_n_players(&app), 1);
     }
 }
