77b8c685a924d7652a7dbc8da64e48bcf1bd5cf9
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-06-29 11:06:25 +0200

    Add player

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,47 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct Paddle;
+
 /// Create a Bevy app
 pub fn create_app() -> App {
-    return App::new();
+    let mut app = App::new();
+    app.add_systems(Startup, setup_paddle);
+    app.update();
+    return app;
+}
+
+// Add the paddle to our world
+fn setup_paddle(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(100.0, 20.0, 1.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: Color::ORANGE,
+                ..default()
+            },
+            ..default()
+        },
+        Paddle,
+    ));
+}
+
+#[cfg(test)]
+fn count_n_paddles(app: &App) -> usize {
+    let mut n = 0;
+    println!("Number of components: {}", app.world.components().len());
+    for c in app.world.components().iter() {
+        // The complete name will be '[crate_name]::Paddle'
+        if c.name().contains("::Paddle") {
+            n += 1;
+        }
+        println!("Name: {}", c.name());
+    }
+    return n;
 }
 
 #[cfg(test)]
@@ -17,5 +56,26 @@ mod tests {
     #[test]
     fn test_can_create_app() {
         create_app();
+    }
+
+    #[test]
+    fn test_empty_app_has_no_paddles() {
+        let app = App::new();
+        assert_eq!(count_n_paddles(&app), 0);
+    }
+
+    #[test]
+    fn test_setup_paddle_adds_a_paddle() {
+        let mut app = App::new();
+        assert_eq!(count_n_paddles(&app), 0);
+        app.add_systems(Startup, setup_paddle);
+        app.update();
+        assert_eq!(count_n_paddles(&app), 1);
+    }
+
+    #[test]
+    fn test_create_app_has_a_paddle() {
+        let app = create_app();
+        assert_eq!(count_n_paddles(&app), 1);
     }
 }
