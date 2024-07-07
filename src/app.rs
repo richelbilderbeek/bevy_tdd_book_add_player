6ea24a3306ff0061a25252e2133566432c951141
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-07 14:41:00 +0200

    Simplify

--- src/app.rs
+++ src/app.rs
@@ -31,11 +31,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_testing() {
-        assert_eq!(1 + 1, 2)
-    }
-
-    #[test]
     fn test_can_create_app() {
         create_app();
     }
