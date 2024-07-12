f38bbb86ffffcb9591825c0bfadf8fdb56c4ad75
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-12 10:51:15 +0200

    Update chapters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use bevy::prelude::*;
 use crate::app::create_app;
 mod app;
 
 fn main() {
     let mut app = create_app();
-    app.add_plugins(bevy::prelude::DefaultPlugins);
+    app.add_plugins(DefaultPlugins);
     app.run();
 }
